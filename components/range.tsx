b97e54d88ac209af4a3c22c972c537da0c922958
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-05-03 23:03:03 +0900

    [mod]change labeled input
    add label and fix

--- components/range.tsx
+++ components/range.tsx
@@ -1,15 +1,19 @@
 
-type Props = JSX.IntrinsicElements['input']
+type Props = JSX.IntrinsicElements['input'] & { children: string }
 
-export const Range = (props: Props) => (
-  <div>
-    <input
-      type="range"
-      list="marks"
-      { ...props }
-    />
-    <datalist id="marks">
-      { [...Array(props?.max ?? 100).keys()].map(n => <option key={ n.toString() } value={ n } />) }
-    </datalist>
-  </div>
-)
+export const Range = (props: Props) => {
+  const { children, ...inputProps } = props
+  return (
+    <label>
+      { children }
+      <input
+        type="range"
+        list="marks"
+        { ...inputProps }
+      />
+      <datalist id="marks">
+        { [...Array(props?.max ?? 0).keys()].map(n => <option key={ n.toString() } value={ n } />) }
+      </datalist>
+    </label>
+  )
+}
