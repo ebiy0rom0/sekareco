28c3aa78d25b1d1552503030a312c113e5d5af23
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-05-03 17:33:16 +0900

    [mod]fix component
    not use enum
    set class name
    review display

--- components/clear.tsx
+++ components/clear.tsx
@@ -1,15 +1,24 @@
 
-export enum ClearStatus {
-  NOPLAY,
-  CLEAR,
-  FULLCOMBO,
-  ALLPERFECT
+export const ClearStatus = {
+  NOPLAY:     0,
+  CLEAR:      1,
+  FULLCOMBO:  2,
+  ALLPERFECT: 3
+} as const
+
+type T = typeof ClearStatus[keyof typeof ClearStatus]
+
+const classSet = {
+  [ClearStatus.NOPLAY]:     'c-noplay',
+  [ClearStatus.CLEAR]:      'c-clear',
+  [ClearStatus.FULLCOMBO]:  'c-fc',
+  [ClearStatus.ALLPERFECT]: 'c-ap'
+}
+type Props = {
+  class?: string
+  status: T
 }
 
-declare type cProps = {
-  status: ClearStatus
-}
-
-export const Clear = () => (
-  <p>coming soon...</p>
+export const Clear = (props: Props) => (
+  <p className={ props?.class }>{ props.status }</p>
 )
