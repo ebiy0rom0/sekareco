a93d8ca21b58fde0062103476c64c9d36cb70acd
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2022-05-17 00:23:37 +0900

    [add]make api factory
    can call any api

---
+++ api/apiFactory.ts
@@ -0,0 +1,19 @@
+import { musicRepository } from './repository/musicRepository.ts'
+import { personRepository } from './repository/personRepository.ts'
+import { recordRepository } from './repository/recordRepository.ts'
+
+type Repositories = {
+  music:  typeof musicRepository,
+  person: typeof personRepository,
+  record: typeof recordRepository
+}
+
+const repo: Repositories = {
+  music:  musicRepository,
+  person: personRepository,
+  record: recordRepository,
+}
+
+export const apiFactory = {
+  get: <T extends keyof Repositories>(target: T): Repositories[T] => repo[target]
+}
