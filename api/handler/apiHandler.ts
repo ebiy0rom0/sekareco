cdfb5b3fa8b947247d310169ceee47e64a2c8989
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2022-05-17 00:20:18 +0900

    [add]create api handler
    using ky

---
+++ api/handler/apiHandler.ts
@@ -0,0 +1,16 @@
+import ky from 'https://cdn.skypack.dev/ky?dts'
+
+const api = ky.create({
+  prefixUrl: 'http://localhost:8080/'
+})
+
+export const apiHandler = api.extend({
+  hooks: {
+    beforeRequest: [
+      request => {
+        request.headers.set('Content-Type', 'application/json');
+      }
+    ]
+  }
+})
+
