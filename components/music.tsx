4ef2593ab6bfb687ae073eb0ab84a83b73683b6b
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2022-05-24 02:07:27 +0900

    [mod]paformance tuning
    music component support for React.memo
    because a lot of change and render content

--- components/music.tsx
+++ components/music.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export const Music = (props: Props) => (
+export const Music = React.memo((props: Props) => {
+  console.log('render music: ' + props.title)
+  return (
   <div className={ props?.class }>
     <img height='50' width='50' src={ props.url } />
     <p>{ props.title }</p>
   </div>
-)
+)})
 
 type Props = {
   class?: string
