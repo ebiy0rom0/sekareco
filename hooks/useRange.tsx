d874d7c8d6c80fdbd30ba7b0602d20dbfc67bd72
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2022-05-24 02:02:56 +0900

    [add]make custom hook
    for input range

---
+++ hooks/useRange.tsx
@@ -0,0 +1,17 @@
+import { useState, useCallback } from 'react'
+
+export const useRange = (init: number) => {
+  const [ range, setRange ] = useState<number>(init)
+
+  // setter wrap
+  const changeRange = (val: number, lower: number, upper: number) => {
+    // rounding to args range
+    const newVal = val < lower ? lower : val > upper ? upper : val
+    setRange(newVal)
+  }
+
+  return {
+    range: () => range,
+    changeRange
+  }
+}
