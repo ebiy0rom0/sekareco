0140eb9f5295b36a7a52ad8ed70e1e38589e9d85
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-05-30 01:52:09 +0900

    [mod]change design
    model rhombus in svg

--- components/clear.tsx
+++ components/clear.tsx
@@ -1,9 +1,9 @@
 import { ClearStatusList, ClearStatusValues } from '../hooks/useRecord.tsx'
 
 export const Clear = (props: Props) => (
-  <p className={ [props?.class, classSet[props.status]].join(' ') }>
-    { props.status }
-  </p>
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fillOpacity="1.0">
+    <polygon points="0,10 10,0 20,10 10,20" className="stroke-2 stroke-gray-900 fill-yellow-300" />
+  </svg>
 )
 
 type Props = {
@@ -12,8 +12,8 @@ type Props = {
 }
 
 const classSet = {
-  [ClearStatusList.NOPLAY]:      'c-noplay',
-  [ClearStatusList.CLEAR]:       'c-clear',
-  [ClearStatusList.FULL_COMBO]:  'c-fc',
-  [ClearStatusList.ALL_PERFECT]: 'c-ap'
+  [ClearStatusList.NOPLAY]:      'record__noplay',
+  [ClearStatusList.CLEAR]:       'record__clear',
+  [ClearStatusList.FULL_COMBO]:  'record__full-combo',
+  [ClearStatusList.ALL_PERFECT]: 'record__all-perfect'
 }
