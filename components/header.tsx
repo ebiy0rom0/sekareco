52d4069ae3ea4d19719c2df1d639c03c19a72382
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-05-31 01:35:21 +0900

    [mod]change GitHub logo
    change svg format

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export const Header = React.memo(() => (
     <h3 className="header__title m-0">プロセカ クリア記録</h3>
     <div className="ml-auto">
       <a href="https://github.com/ebiy0rom0/sekareco">
-        <img src="/assets/GitHub-Mark-Light-64px.png" height="25" title="GitHub Repository" />
+        <img src="/assets/github.svg" height="24" title="GitHub" className="invert" />
       </a>
     </div>
   </div>
