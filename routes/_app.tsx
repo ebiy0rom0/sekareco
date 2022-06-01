2c1d5aafe788b475665cf46fa9fbd1bdd1f3566d
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-06-02 02:26:15 +0900

    [mod]change base layout
    max width extend

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,21 +4,19 @@ import { Navigation } from "../components/navigation.tsx"
 
 const App: React.FC<Props> = props => {
   return (
-    <div className="w-screen h-screen bg-slate-800 text-slate-400">
+    <div className="bg-slate-800 text-slate-400">
       <Head>
         <title>プロセカの記録帳</title>
       </Head>
       <div className="sticky top-0 w-full flex-none border-b border-slate-500/40">
-        <div className="max-w-7xl py-3 mx-auto">
+        <div className="max-w-[100em] px-10 py-3 mx-auto">
           <Header />
         </div>
       </div>
-      <div className="max-w-7xl mx-auto mt-8">
-        <Navigation className="w-[15em] fixed justify-items-center border-r-2 border-slate-400/20 ml-5" />
-        <div className="pl-[15em] w-auto grid grid-cols-7">
-          <div className="col-start-2 col-end-8">
-            { props.children }
-          </div>
+      <div className="max-w-[100em] mx-auto py-8">
+        <Navigation className="w-[13em] fixed justify-items-center border-r-2 border-slate-400/20 ml-5" />
+        <div className="ml-5 pl-[13em] grid w-auto justify-center">
+          { props.children }
         </div>
       </div>
     </div>
