3b8a8009f48cfe9c87f7567b1f359d0cb5728422
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-06-13 01:55:42 +0900

    [mod]change return types
    to product use

--- api/repository/musicRepository.ts
+++ api/repository/musicRepository.ts
@@ -1,8 +1,14 @@
+/// <reference types="./../../types/index.d.ts" />
 import { apiHandler } from "../handler/apiHandler.ts"
 
 export const musicRepository = {
   getMusicList: async () => {
-    const json = await apiHandler.get("music/").json<string>()
+    const json = await apiHandler.get("music/")
+      .json<M_Music.Music[]>()
+      .catch<M_Music.Music[]>(e => {
+        console.log("failed to get music master: " + e)
+        return []
+      })
 
     return json
   }
--- api/repository/recordRepository.ts
+++ api/repository/recordRepository.ts
@@ -1,19 +1,26 @@
+/// <reference types="./../../types/index.d.ts" />
+import { ClearStatusValues } from "./../../hooks/useRecord.ts"
 import { apiHandler } from "../handler/apiHandler.ts"
 
 export const recordRepository = {
   getMyRecord: async (personId: number) => {
-    const json = await apiHandler.get(`record/${personId}/`).json<string>()
-    console.log(json)
+    const json = await apiHandler.get(`record/${personId}/`)
+      .json<P_Record.Record<ClearStatusValues>>()
+      .catch<P_Record.Record<ClearStatusValues>>(e => {
+        // TODO: display common error view
+        console.log(e)
+        return {}
+      })
 
     return json
   },
+
   registRecord: async (personId: number, musicId: number, record: number[]) => {
     const json = await apiHandler.post(`record/${personId}/${musicId}/`, {
       json: {
         record: record[0]
       }
-    }).json<string>()
-    console.log(json)
+    }).json<ClearStatusValues[]>()
 
     return json
   }
