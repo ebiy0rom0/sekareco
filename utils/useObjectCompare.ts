c9a8a5895ab6d078880edf482a9c53ba5a5d55b9
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2022-06-13 13:59:28 +0900

    [mod]fix object compare hook
    fixed a bug that prevented working with added any items to `obj2`

---
+++ utils/useObjectCompare.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react"
+
+export const useObjectCompare = (obj1: Record<string, unknown>, obj2: Record<string, unknown>) => {
+  const [ difference, setDifference ] = useState(false)
+
+  // true: same struct and value
+  // false: difference struct or value
+  const compare = (obj1: Record<string, unknown>, obj2: Record<string, unknown>): boolean => {
+    // compare object sort keys
+    if (JSON.stringify(Object.keys(obj1).sort()) !== JSON.stringify(Object.keys(obj1).sort())) return true
+
+    return Object.entries(obj1).map(([key, val]) => {
+      // case: types unmatch
+      if (typeof val !== typeof obj2[key]) return false
+
+      if (Array.isArray(val)) {
+        // case: array
+        return JSON.stringify(val) === JSON.stringify(obj2[key])
+
+      } else if (typeof val === "object") {
+        // case: object
+        return compare(val as Record<string, unknown>, obj2[key] as Record<string, unknown>)
+
+      } else {
+        // case: primitive
+        return val === obj2[key]
+      }
+    }).some(b => !b)  // if even once difference return `true`
+  }
+
+  useEffect(() => {
+    setDifference(compare(obj1, obj2))
+  }, [obj1, obj2])
+
+  return { difference: () => difference }
+}
