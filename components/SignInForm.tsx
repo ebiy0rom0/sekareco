d0015bd157e45c874ae9d6b4836bb062003fb961
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2022-06-16 15:32:24 +0900

    [add]make form component
    ? same content form

---
+++ components/SignInForm.tsx
@@ -0,0 +1,15 @@
+import { useInput } from "../hooks/useInput.ts"
+import { Input } from "./Input.tsx"
+
+export const SignInForm = () => {
+  const [ _loginID, setLoginID ] = useInput("")
+  const [ _password, setPassword ] = useInput("")
+
+  return (
+    <>
+      <Input labelName="login id" type="text" onChange={ setLoginID } />
+      <Input labelName="password" type="password" onChange={ setPassword } />
+      <button>sign in</button>
+    </>
+  )
+}
