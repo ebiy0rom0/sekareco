f43954a1d3ef7f91984b41e8295004b556da7bb6
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-06-18 23:32:58 +0900

    [mod]layout change
    Button component use
    style set

--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -1,3 +1,5 @@
+import { Link } from "aleph/react"
+import { Button } from "../components/Button.tsx"
 import { useInput } from "../hooks/useInput.ts"
 import { Input } from "./Input.tsx"
 
@@ -6,10 +8,12 @@ export const SignInForm = () => {
   const [ _password, setPassword ] = useInput("")
 
   return (
-    <>
-      <Input labelName="login id" type="text" onChange={ setLoginID } />
-      <Input labelName="password" type="password" onChange={ setPassword } />
-      <button>sign in</button>
-    </>
+    <div className="flex flex-col gap-y-7">
+      <Input id="loginId" labelName="login id" type="text" onChange={ setLoginID } />
+      <Input id="password" labelName="password" type="password" onChange={ setPassword } />
+      <Link to="/records">
+        <Button className="text-white-200 bg-blue-600 hover:bg-blue-400">sign in</Button>
+      </Link>
+    </div>
   )
 }
