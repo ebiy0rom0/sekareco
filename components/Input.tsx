2fe44678a0ce582dc1fb955cd34158c3f23f4417
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-06-18 23:31:34 +0900

    [mod]change layout
    support placeholder

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,15 +1,24 @@
 import React from "react"
 
 export const Input = (props: Props) => (
-  <label>
-    { props.labelName }
-    <input type={ props.type } value={ props?.value } onChange={ props.onChange } />
+  <label htmlFor={ props.id }>
+    <strong>{ props.labelName }</strong>
+    <input
+      id={ props.id }
+      className="block mt-2 py-2 px-3 rounded-lg w-fit border-slate-200 focus:outline-sky-500/80"
+      type={ props.type }
+      value={ props?.value }
+      onChange={ props.onChange }
+      placeholder={ props?.placeholder }
+    />
   </label>
 )
 
 type Props = {
+  id: string
   labelName: string
   type: "text" | "password"
   value?: string
+  placeholder?: string
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
