023f0afb8b6adcdd2c685903e5ce1744370acfab
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-06-18 23:34:13 +0900

    [mod]layout change
    api test button deleted

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,4 @@
 import { Link } from "aleph/react"
-import { Input } from "../components/Input.tsx"
 import { SignInForm } from "../components/SignInForm.tsx"
 import { SignUpForm } from "../components/SignUpForm.tsx"
 import { Tab } from "../components/Tab.tsx"
@@ -8,49 +7,24 @@ import { useInput } from "../hooks/useInput.ts"
 import { useLog } from "../hooks/useLog.tsx"
 import { useDelayCallback } from "../hooks/useDelayCallback.ts"
 
-const linkIcon = (
-  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M14 5C13.4477 5 13 4.55228 13 4C13 3.44772 13.4477 3 14 3H20C20.2652 3 20.5196 3.10536 20.7071 3.29289C20.8946 3.48043 21 3.73478 21 4L21 10C21 10.5523 20.5523 11 20 11C19.4477 11 19 10.5523 19 10L19 6.41422L9.70711 15.7071C9.31658 16.0976 8.68342 16.0976 8.29289 15.7071C7.90237 15.3166 7.90237 14.6834 8.29289 14.2929L17.5858 5H14ZM3 7C3 5.89543 3.89543 5 5 5H10C10.5523 5 11 5.44772 11 6C11 6.55228 10.5523 7 10 7H5V19H17V14C17 13.4477 17.4477 13 18 13C18.5523 13 19 13.4477 19 14V19C19 20.1046 18.1046 21 17 21H5C3.89543 21 3 20.1046 3 19V7Z"
-      fill="currentColor"
-    />
-  </svg>
-);
-
 const Index: React.FC = () => {
   const { setLog, renderLog } = useLog()
-  const { start, stop } = useDelayCallback(10000, () => alert("test"))
-  const [ loginID, handleLoginID ] = useInput("")
+
   return (
-    <div className="flex flex-row">
-      <div className="w-2/6">
-        <Input labelName="login ID" type="text" onChange={ handleLoginID } />
-        <h1 className="ml-3">
-          The Fullstack Framework in Deno.
+    <div className="flex flex-row w-full">
+      <div className="basis-3/6">
+        <h1>
+          プロセカの記録帳
         </h1>
         <p>
-          <strong>Aleph.js</strong> gives you the best developer experience for building web applications<br />{" "}
-          with modern toolings.
+          記録残せるよ
         </p>
-        <nav>
-          <button onClick={async () => setLog(await apiFactory.get("music").getMusicList())}>get music</button>
-          <button onClick={async () => setLog(await apiFactory.get("person").login(1, "hoge"))}>person login</button>
-          <button onClick={async () => setLog(await apiFactory.get("person").registPerson("hoge", "hoge0123", "huga"))}>regist person</button>
-          <button onClick={async () => setLog(await apiFactory.get("person").modifyPersonStatus(1, "piyo", "hoge3210"))}>modify person</button>
-          <button onClick={async () => setLog(await apiFactory.get("record").getMyRecord(1))}>get record</button>
-          <button onClick={async () => setLog(await apiFactory.get("record").registRecord(1, 1, [0]))}>regist record</button>
-          <button onClick={() => start()}>delay test start</button>
-          <button onClick={() => stop()}>delay test stop</button>
-          <Link to="/records">
-            <button onClick={() => console.log("no entry")}>Your Records</button>
-          </Link>
-        </nav>
       </div>
-      <div className="border-5 p-7 m-5 rounded-2xl w-full">
+      <div className="border-5 p-6 m-5 rounded-2xl w-full">
         <Tab tabs={
           [
-            {title: "sign in", key:"sign-in", content: (<SignInForm />)},
-            {title: "sign up", key:"sign-up", content: (<SignUpForm />)},
+            {title: "サインイン", key:"sign-in", content: (<SignInForm />)},
+            {title: "サインアップ", key:"sign-up", content: (<SignUpForm />)},
           ]
         } />
       </div>
