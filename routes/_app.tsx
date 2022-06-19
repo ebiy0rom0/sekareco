75d5cf9077eee80b8bdaad8681d7f37d102857c3
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-06-20 03:07:55 +0900

    [wip]layout change
    if not authorized, not display header and side navigation

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -12,24 +12,8 @@ const App: React.FC<Props> = props => {
       <Head>
         <title>プロセカの記録帳</title>
       </Head>
-      <div className="sticky top-0 w-full flex-none border-b border-slate-500/40">
-        <div className="max-w-[100em] px-10 py-3 mx-auto">
-          <Header />
-        </div>
-      </div>
-      <div className="max-w-[100em] mx-auto py-8">
-        <Navigation
-          className="
-            fixed
-            box-border
-            w-[13em]
-            min-h-screen
-            px-6 py-5
-            justify-center
-            border-r-2 border-slate-400/20
-          "
-        />
-        <div className="ml-5 pl-[13em] flex flex-col items-center">
+      { true ? (
+        <div className="max-w-[100em] mx-auto py-8 flex flex-col items-center">
           { props.children }
           <div
             className="
@@ -41,7 +25,40 @@ const App: React.FC<Props> = props => {
             <Footer />
           </div>
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="sticky top-0 w-full flex-none border-b border-slate-500/40">
+            <div className="max-w-[100em] px-10 py-3 mx-auto">
+              <Header />
+            </div>
+          </div>
+          <div className="max-w-[100em] mx-auto py-8">
+            <Navigation
+              className="
+                fixed
+                box-border
+                w-[13em]
+                min-h-screen
+                px-6 py-5
+                justify-center
+                border-r-2 border-slate-400/20
+              "
+            />
+            <div className="ml-5 pl-[13em] flex flex-col items-center">
+              { props.children }
+              <div
+                className="
+                  border-t border-slate-300/20
+                  mt-10 pt-15 pb-7
+                  w-full
+                "
+              >
+                <Footer />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   )
 }
