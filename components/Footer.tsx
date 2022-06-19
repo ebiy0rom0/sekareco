eb0ca8a6e71d555754e39ea572541899078cc646
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-06-19 22:57:41 +0900

    [mod]footer display copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react"
 
 export const Footer = React.memo(() => (
-  <div>footer</div>
+  <div className="text-center text-sm">Copyright © 2022 ebiy0rom0. All Rights Reserved.</div>
 ))
