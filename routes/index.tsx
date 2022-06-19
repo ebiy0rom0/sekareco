ed5fead152592afdc944fbad61caedb874760e59
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-06-19 23:00:14 +0900

    [mod]index page layout change

--- routes/index.tsx
+++ routes/index.tsx
@@ -11,20 +11,27 @@ const Index: React.FC = () => {
   const { setLog, renderLog } = useLog()
 
   return (
-    <div className="flex flex-row w-full">
-      <div className="basis-3/6">
-        <h1>
-          プロセカの記録帳
-        </h1>
+    <div className="flex flex-row min-h-[75vh] w-4/5 justify-center place-items-center">
+      <div className="flex-1">
+        <div className="flex gap-x-4">
+          { "プロセカの".split("").map(s => (
+            <h1><span className="text-6xl">{ s }</span></h1>
+          )) }
+        </div>
+        <div className="flex gap-x-4">
+          { "記録帳".split("").map(s => (
+            <h1><span className="text-6xl">{ s }</span></h1>
+          )) }
+        </div>
         <p>
           記録残せるよ
         </p>
       </div>
-      <div className="border-5 p-6 m-5 rounded-2xl w-full">
+      <div className="border-4 p-6 m-5 rounded-2xl flex-1 bg-slate-800/90">
         <Tab tabs={
           [
-            {title: "サインイン", key:"sign-in", content: (<SignInForm />)},
-            {title: "サインアップ", key:"sign-up", content: (<SignUpForm />)},
+            {title: "sign in", key:"sign-in", content: (<SignInForm />)},
+            {title: "sign up", key:"sign-up", content: (<SignUpForm />)},
           ]
         } />
       </div>
