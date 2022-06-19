92b709c0fe647ffe1c41e7c3fd43afae353a9c60
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-06-20 03:09:23 +0900

    [add]make component
    github icon component

---
+++ components/GithubIcon.tsx
@@ -0,0 +1,7 @@
+
+export const GithubIcon = () => (
+  <a href="https://github.com/ebiy0rom0/sekareco">
+    <span className="sr-only">Link to GitHub</span>
+    <img src="/assets/github.svg" height="24" title="GitHub" className="invert hover:opacity-80" />
+  </a>
+)
