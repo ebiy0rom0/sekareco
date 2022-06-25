a429e0d725d9a65d8998ae14563774b2f0302963
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-06-25 23:33:01 +0900

    [del]refactor
    semi delete

--- api/handler/apiHandler.ts
+++ api/handler/apiHandler.ts
@@ -8,7 +8,7 @@ export const apiHandler = api.extend({
   hooks: {
     beforeRequest: [
       request => {
-        request.headers.set("Content-Type", "application/json");
+        request.headers.set("Content-Type", "application/json")
       }
     ]
   }
