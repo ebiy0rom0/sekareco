d74d9554a2b1d5e73d31a5c84c57619e3b89ad84
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2022-06-28 19:26:20 +0900

    [mod]type specific
    type specification for return value

--- hooks/useInput.ts
+++ hooks/useInput.ts
@@ -1,6 +1,9 @@
 import React, { useState } from "react"
 
-export const useInput = (defaultValue: string) => {
+export const useInput = (defaultValue: string): [
+  () => string,
+  (e: React.ChangeEvent<HTMLInputElement>) => void
+] => {
   const [ value, setValue ] = useState<string>(defaultValue)
 
   const handle = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)
