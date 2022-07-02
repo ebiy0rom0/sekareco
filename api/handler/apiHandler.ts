1bdcb4c67e3ea9a16bbfd3dd091eedb8cc64b404
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-07-02 18:51:33 +0900

    [mod]make a authed handler

--- api/handler/apiHandler.ts
+++ api/handler/apiHandler.ts
@@ -1,16 +1,36 @@
 import ky from "https://cdn.skypack.dev/ky?dts"
 
-const api = ky.create({
-  prefixUrl: "http://localhost:8000/"
-})
-
-export const apiHandler = api.extend({
+export const apiHandler = ky.create({
+  prefixUrl: "http://localhost:8000/",
   hooks: {
     beforeRequest: [
       request => {
         request.headers.set("Content-Type", "application/json")
       }
+    ],
+    afterResponse: [
+      async (_request, _options, response) => {
+        if (response.status === 503) {
+          const body = await response.json() as {'error': string}
+          console.log(body.error)
+        }
+      }
     ]
   }
 })
 
+export let authedHandler: typeof apiHandler | undefined = undefined
+
+export const setAuth = (token: string) => {
+  authedHandler = apiHandler.extend({
+    hooks: {
+      beforeRequest: [
+        request => {
+          request.headers.set("Authorizetion", `Bearer ${token}`)
+        }
+      ]
+    }
+  })
+}
+
+export const resetAuth = () => authedHandler = undefined
