eb372e3b377d91a0ccb0c092beb132e8e078a957
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-07-17 02:25:44 +0900

    [mod]input layout change

--- components/Input.tsx
+++ components/Input.tsx
@@ -5,23 +5,22 @@ export const Input = (props: Props) => (
   <ThemeCtx.Consumer>
     { ({ darkMode }) => (
       <label htmlFor={ props.id } className={ props?.className }>
-        <strong>{ props.labelName }</strong>
+        <strong className="ms-1">{ props.labelName }</strong>
         <input
           id={ props.id }
           className={ [
             "block box-border",
-            "text-lg",
-            "text-slate-300/80",
+            "text-lg tracking-widest",
+            darkMode ? "text-slate-300/80" : "text-slate-700/80",
             "w-full",
             "mt-2 py-1 px-3",
             "rounded-lg shadow-sm border-none",
-            darkMode ? "bg-slate-800" : "bg-slate-300",
+            darkMode ? "bg-slate-700" : "bg-slate-300/70",
             "placeholder-gray-900/40",
-            darkMode ? "ring-1 ring-slate-500" : "ring-1 ring-slate-400",
             "focus:outline-none",
-            darkMode ? "focus:bg-slate-800/90" : "focus:bg-slate-200",
+            darkMode ? "focus:bg-slate-600/90" : "focus:bg-slate-200/90",
             "focus:caret-sky-500",
-            "focus:ring-3 focus:ring-sky-500",
+            "focus:ring-2 focus:ring-sky-700",
             "peer"
           ].join(" ") }
           type={ props.type }
