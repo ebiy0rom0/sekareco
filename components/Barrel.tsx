4fee1e3b2dd834842d8f8d69759820c044f59352
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-07-17 03:25:42 +0900

    [mod]fix component name

--- components/Barrel.tsx
+++ components/Barrel.tsx
@@ -1,5 +1,5 @@
 
-export const Toggle = (props: Props) => (
+export const Barrel = (props: Props) => (
   <div className="flex gap-x-3">
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" onClick={ () => props.decrement() }>
       <polyline points="10,0 0,10 10,20 10,15 5,10 10,5" className="fill-pink-500/50" />
