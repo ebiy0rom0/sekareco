51bcbfba1622702bcbdea0453b279e6eabbcbc15
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-07-18 22:56:44 +0900

    [mod]🐛fix if throw HTTPError, app to crash
    should handling(catch) api error

--- api/repository/personRepository.ts
+++ api/repository/personRepository.ts
@@ -2,7 +2,7 @@ import { apiHandler } from "../handler/apiHandler.ts"
 
 export const personRepository = {
   login: async (loginID: string, password: string) => {
-    const json = await apiHandler.post(`person/auth/`, {
+    const json = await apiHandler.post("signin", {
       json: {
         person_id: loginID,
         password: password
@@ -13,15 +13,15 @@ export const personRepository = {
     return json
   },
   registPerson: async (loginID: string, name: string, password: string) => {
-    const json = await apiHandler.post("person/", {
+    const json = await apiHandler.post("signup", {
       json: {
         login_id: loginID,
         password: password,
         person_name: name
       }
-    }).json<string>()
-    console.log(json)
+    }).catch(e => console.log(e))
 
+    console.log(json)
     return json
   },
   modifyPersonStatus: async (personID: number, name?: string, password?: string) => {
