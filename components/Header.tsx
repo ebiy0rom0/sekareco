3dcedc4717723daa131ebe38c22b60362ac61bc5
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-08-11 23:31:58 +0900

    [add]theme toggle add

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,18 @@
 import React from "react"
 import { GithubIcon } from "./GithubIcon.tsx"
+import { ThemeCtx } from "../hooks/useTheme.tsx"
+import { Toggle, ToggleStyle } from "./Toggle.tsx"
 
 export const Header = React.memo(() => (
-  <div className="header flex items-center">
-    <h3 className="header__title m-0">プロセカの記録帳</h3>
-    <div className="ml-auto">
-      <GithubIcon />
-    </div>
-  </div>
+  <ThemeCtx.Consumer>
+    { ({ darkMode, switchMode }) => (
+      <div className="header flex items-center">
+        <h3 className="header__title m-0">プロセカの記録帳</h3>
+        <div className="flex items-end gap-x-10 ml-auto">
+          <Toggle mode={ !darkMode } style={ ToggleStyle.THEME } role={ switchMode } />
+          <GithubIcon />
+        </div>
+      </div>
+    )}
+  </ThemeCtx.Consumer>
 ))
