d2b2b9937c08fde3f7694c5ed545ca414bf05f4e
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2022-08-15 14:40:08 +0900

    [mod]storage reload support
    add reload hook

--- hooks/useLogin.ts
+++ hooks/useLogin.ts
@@ -1,12 +1,13 @@
-import { useCallback } from "react"
+import { useState, useEffect } from "react"
 import { apiFactory } from "../api/apiFactory.ts"
 import { setAuth } from "../api/handler/apiHandler.ts"
 import { useSessionStorage } from "../utils/useSessionStorage.ts"
 
-export const useLogin = () => {
+export const useLogin = (reloadKey = 0) => {
   const key = "token"
-  const [ token, setToken ] = useSessionStorage(key, "")
-
+  const [ token, setToken ] = useSessionStorage(key, "", reloadKey)
+  const [ isLogin, setLogin ] = useState<boolean>(false)
+console.log("use login")
   const tryLogin = async (loginId: string, password: string) => {
     const newToken = await apiFactory.get("person").login(loginId, password)
     if (newToken.length > 0) {
@@ -18,12 +19,13 @@ export const useLogin = () => {
     return false
   }
 
-  // login check
-  const isLogin = () => token().length > 0
+  useEffect(() => {
+    setLogin(token.length > 0)
+  }, [token])
 
-  if (isLogin()) {
+  if (isLogin) {
     console.log("set auth")
-    setAuth(token())
+    setAuth(token)
   }
   return {
     token,
--- utils/useSessionStorage.ts
+++ utils/useSessionStorage.ts
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react"
 
 export const useSessionStorage = <T>(
   key: string,
-  defaultValue: T
-): [() => T, React.Dispatch<React.SetStateAction<T>>] => {
+  defaultValue: T,
+  reloadKey = 0
+): [T, React.Dispatch<React.SetStateAction<T>>] => {
   const [ storage, serializer, parser ] = [ window?.sessionStorage, JSON.stringify, JSON.parse ]
 
   // server side useless
@@ -12,24 +13,33 @@ export const useSessionStorage = <T>(
 
   const [ storedValue, setValue ] = useState<T>(() => {
     if (!availableStorage()) return defaultValue
-
     try {
       const item = storage.getItem(key)
-      console.log(`key: ${key}, item: ${item}`)
       return item ? parser(item) : defaultValue
-
     } catch (_) {
       console.log(errorMsg)
     }
   })
 
+  // reload hook
+  // when changed reload key, get storage again by key
   useEffect(() => {
     if (!availableStorage()) return
+    try {
+      const item = storage.getItem(key)
+      setValue(item ? parser(item) : storedValue)
+    } catch (_) {
+      console.log(errorMsg)
+    }
+  }, [reloadKey])
 
+  useEffect(() => {
+    if (!availableStorage()) return
     try {
       if (storedValue) {
         storage.setItem(key, serializer(storedValue))
       } else {
+        // when updated value empty, remove by storage
         storage.removeItem(key)
       }
     } catch (_) {
@@ -38,7 +48,7 @@ export const useSessionStorage = <T>(
   }, [storedValue])
 
   return [
-    () => storedValue,
+    storedValue,
     setValue
   ]
 }
