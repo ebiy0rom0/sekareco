0b8114c941113c6d3adcab7ac47a9a858687a819
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-08-20 23:58:20 +0900

    [mod]types

---
+++ types/index.ts
@@ -0,0 +1,20 @@
+
+export const clearStatus = {
+  NOPLAY: 0,
+  CLEAR:  1,
+  FULL_COMBO: 2,
+  ALL_PERFECT: 3
+} as const
+
+export type ClearStatus = typeof clearStatus[keyof typeof clearStatus]
+
+
+export const difficulty = {
+  EASY: 0,
+  NORMAL: 1,
+  HARD: 2,
+  EXPERT: 3,
+  MASTER: 4
+} as const
+
+export type Difficulty = typeof difficulty[keyof typeof difficulty]
