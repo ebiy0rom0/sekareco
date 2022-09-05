f2edb7b67e60b48a487ea4d429c726f9add13804
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2022-09-05 23:42:00 +0900

    [mod]fix to size

--- components/GithubIcon.tsx
+++ components/GithubIcon.tsx
@@ -6,7 +6,7 @@ export const GithubIcon = () => (
     { ({ darkMode }) => (
       <a href="https://github.com/ebiy0rom0/sekareco">
         <span className="sr-only">Link to GitHub</span>
-        <img src="/assets/github.svg" height="24" title="GitHub" className={ "hover:opacity-80 fill-blue-500" + (darkMode ? " invert" : "") } />
+        <img src="/assets/github.svg" height="30" title="GitHub" className={ "hover:opacity-80 fill-blue-500" + (darkMode ? " invert" : "") } />
       </a>
     )}
   </ThemeCtx.Consumer>
