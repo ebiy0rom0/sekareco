db36e4efd21c5b05cfa3c163aa72c9311973ab32
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2022-09-24 01:36:22 +0900

    [mod]theme support

--- hooks/useModal.tsx
+++ hooks/useModal.tsx
@@ -1,44 +1,51 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
+import { ThemeCtx } from "~/hooks/useTheme.tsx"
 
 export const useModal = () => {
   const [ open, setOpen ] = useState(false)
 
-  const render = (children: React.ReactNode) => {
-    if (typeof document === "undefined") return 0
-    return (
-    <div
-      className={`
-        items-center fixed grid inset-0
-        ${ open ? "opacity-100" : "pointer-events-none opacity-0" }
-        transition-opacity
-        ease-in-out
-        duration-300
-      `}
-    >
-      <div
-        className={`
-          flex place-self-center z-10 p-7 bg-white shadow-lg w-fit h-fit
-          rounded-lg
-          ${ open ? "opacity-100" : "pointer-events-none opacity-0" }
-          transition-opacity
-          ease-in-out
-          duration-300
-        `}
-      >
-        { children }
-      </div>
-      <div
-        className={`
-          fixed inset-0 bg-indigo-900
-          ${ open ? "opacity-60" : "pointer-events-none opacity-0" }
-          transition-opacity
-          ease-in-out
-          duration-300
-        `}
-        onClick={ () => setOpen(!open) }
-      />
-    </div>
-  )}
+  const render = (children: React.ReactNode) => (
+    <ThemeCtx.Consumer>
+      { ({ darkMode }) => (
+        <div
+          className={`
+            items-center fixed grid inset-0
+            ${ open ? "opacity-100" : "pointer-events-none opacity-0" }
+            transition-opacity
+            ease-in-out
+            duration-300
+          `}
+        >
+          <div
+            className={`
+              flex place-self-center p-7 z-10
+              w-fit h-fit
+              shadow-lg rounded-lg
+              ${ open ? "opacity-100" : "pointer-events-none opacity-0" }
+              ${ darkMode ? "bg-slate-800" : "bg-slate-100" }
+              transition-opacity
+              ease-in-out
+              duration-300
+            `}
+          >
+            { children }
+          </div>
+          <div
+            className={`
+              fixed inset-0
+              ${ open ? "opacity-40" : "pointer-events-none opacity-0" }
+              ${ darkMode ? "bg-slate-200" : "bg-slate-700" }
+              transition-opacity
+              ease-in-out
+              duration-300
+            `}
+            onClick={ () => setOpen(!open) }
+          />
+        </div>
+      ) }
+    </ThemeCtx.Consumer>
+  )
+
   return {
     render,
     open: () => setOpen(!open)
