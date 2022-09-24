6dab178c59f2fedaf2e8923bc960d757dc2854e6
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-09-25 01:26:27 +0900

    [mod]fix size

--- components/atoms/Toggle.tsx
+++ components/atoms/Toggle.tsx
@@ -1,9 +1,9 @@
 // import styles from "~/style/app.css"
 
 export const Toggle = (props: Props) => (
-  <div className={ "grid gap-x-3 w-[45px] h-[25px] rounded-full " + (props.mode ? "justify-items-start bg-gray-300" : "justify-items-end bg-gray-600") } onClick={ props.role }>
+  <div className={ "grid gap-x-3 w-[55px] h-[30px] rounded-full " + (props.mode ? "justify-items-start bg-gray-300" : "justify-items-end bg-gray-600") } onClick={ props.role }>
     <div
-      className={ "grid place-items-center rounded-full bg-slate-700 w-[25px] scale-120 ring-1 " + (props.mode ? "bg-white toggle-on" : "bg-slate-900 toggle-off") }
+      className={ "grid place-items-center rounded-full bg-slate-700 w-[30px] scale-110 ring-1 " + (props.mode ? "bg-white toggle-on" : "bg-slate-900 toggle-off") }
     >
       <Selector style={ props.style } mode={ props.mode } />
     </div>
