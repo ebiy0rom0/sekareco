d704f7f24940f3489eab0398eaf6590c95379e85
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-09-26 01:24:02 +0900

    [mod]change layout

--- components/organisms/Navigation.tsx
+++ components/organisms/Navigation.tsx
@@ -3,19 +3,18 @@ import { Link } from "aleph/react";
 
 export const Navigation = React.memo(() => (
   <>
-    <h2 className="nav__title font-bold">機能 一覧</h2>
-    <ul className="list-none grid gap-y-6 text-xl pl-10">
+    <span className="nav__title text-xl font-bold">機能 一覧</span>
+    <ul className="list-none grid gap-y-5 text-base pl-10 mt-5">
       {[
         ["プロフィール", "/profile"],
         ["記録帳", "/records"],
         ["vs. フレンド", "/versus"],
       ].map(([title, url]) => (
-        <li>
+        <li key={url}>
           <Link
             className="
-              text-slate-400 
-              hover:text-slate-500 
-              font-semibold 
+              hover:opacity-70
+              font-semibold
               decoration-none
             "
             to={url}
