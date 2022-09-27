40a0329537c5389a5fd7bc9c57106ad40ae72194
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-09-28 03:12:11 +0900

    [mod]diff view support

--- components/atoms/Score.tsx
+++ components/atoms/Score.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 
-export const Score = React.memo((props: Props) => (
-  <div className="font-semibold">
-    <span className={`${ props.score < (props.notes * 3) ? "text-rose-400" : "text-green-500" }`}>{props.score}</span>
-    /
-    <span>{props.notes * 3}</span>
-  </div>
-))
+export const Score = React.memo((props: Props) => {
+  const max = (props.notes * 3)
+  const score = props.score ?? 0
+  const prefix = (max - score) > 0 ? "-" : "±"
+
+  return (
+    <div className="font-semibold tracking-wide">
+      <span
+        className={`${
+          score < max
+            ? "text-rose-500"
+            : "text-emerald-500"
+        }`}
+      >
+        {!props?.diff ? score : `${prefix}${max - score}`}
+      </span>
+      /
+      <span>{props.notes * 3}</span>
+    </div>
+  )
+});
 
 type Props = {
-  score: number
-  notes: number
-}
+  score: number;
+  notes: number;
+  diff?: boolean;
+};
