d83f8677e5fad92459d750d81be11bf1137837b1
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-09-27 22:30:08 +0900

    [mod]move testdata

--- api/repository/musicRepository.ts
+++ api/repository/musicRepository.ts
@@ -4,11 +4,232 @@ import { getApiHandler } from "~/api/handler/apiHandler.ts";
 export const musicRepository = {
   getMusicList: async () => {
     // when successfully get, returns music master data
-    const json = await getApiHandler()
-      .get("musics")
-      .json<M_Music.Music[]>()
-      .catch<M_Music.Music[]>((_) => []);
+    // const json = await getApiHandler()
+    //   .get("musics")
+    //   .json<M_Music.Music[]>()
+    //   .catch<M_Music.Music[]>((_) => []);
 
-    return json;
+    // return json;
+    return await testdata
   },
 };
+
+
+const testdata = [
+  {
+    musicID: 1,
+    artistID: 4,
+    musicName: "ECHO",
+    jacketUrl: "/assets/jacket_001.png",
+    level: [6, 12, 16, 23, 26],
+    notes: [157, 279, 404, 659, 805],
+  },
+  {
+    musicID: 2,
+    artistID: 4,
+    musicName: "フラジール",
+    jacketUrl: "/assets/jacket_002.png",
+    level: [6, 11, 17, 24, 27],
+    notes: [108, 377, 669, 863, 1040],
+  },
+  {
+    musicID: 3,
+    artistID: 4,
+    musicName: "Just Be Friends",
+    jacketUrl: "/assets/jacket_003.png",
+    level: [6, 11, 16, 23, 27],
+    notes: [148, 410, 679, 811, 989],
+  },
+  {
+    musicID: 4,
+    artistID: 4,
+    musicName: "幽霊東京",
+    jacketUrl: "/assets/jacket_004.png",
+    level: [7, 13, 17, 24, 27],
+    notes: [184, 375, 595, 904, 1053],
+  },
+  {
+    musicID: 5,
+    artistID: 4,
+    musicName: "群青讃歌",
+    jacketUrl: "/assets/jacket_005.png",
+    level: [7, 13, 17, 24, 27],
+    notes: [199, 344, 475, 711, 833],
+  },
+  {
+    musicID: 6,
+    artistID: 4,
+    musicName: "drop pop candy",
+    jacketUrl: "/assets/jacket_006.png",
+    level: [6, 12, 17, 25, 28],
+    notes: [156, 322, 546, 773, 902],
+  },
+  {
+    musicID: 7,
+    artistID: 4,
+    musicName: "威風堂々",
+    jacketUrl: "/assets/jacket_007.png",
+    level: [6, 12, 17, 24, 28],
+    notes: [188, 326, 604, 975, 1166],
+  },
+  {
+    musicID: 8,
+    artistID: 4,
+    musicName: "トラフィック・ジャム",
+    jacketUrl: "/assets/jacket_008.png",
+    level: [6, 13, 18, 24, 28],
+    notes: [177, 371, 638, 896, 1089],
+  },
+  {
+    musicID: 9,
+    artistID: 4,
+    musicName: "ガランド",
+    jacketUrl: "/assets/jacket_009.png",
+    level: [7, 13, 18, 24, 28],
+    notes: [157, 279, 404, 761, 943],
+  },
+  {
+    musicID: 10,
+    artistID: 4,
+    musicName: "Forward",
+    jacketUrl: "/assets/jacket_010.png",
+    level: [6, 11, 16, 24, 28],
+    notes: [205, 350, 557, 659, 805],
+  },
+  {
+    musicID: 11,
+    artistID: 4,
+    musicName: "Beat Eater",
+    jacketUrl: "/assets/jacket_011.png",
+    level: [6, 11, 17, 25, 28],
+    notes: [118, 251, 481, 772, 798],
+  },
+  {
+    musicID: 12,
+    artistID: 4,
+    musicName: "ミライ",
+    jacketUrl: "/assets/jacket_012.png",
+    level: [6, 12, 18, 24, 28],
+    notes: [184, 419, 662, 1017, 1281],
+  },
+  {
+    musicID: 13,
+    artistID: 4,
+    musicName: "踊",
+    jacketUrl: "/assets/jacket_013.png",
+    level: [6, 13, 18, 26, 29],
+    notes: [242, 409, 640, 882, 1083],
+  },
+  {
+    musicID: 14,
+    artistID: 4,
+    musicName: "雨とペトラ",
+    jacketUrl: "/assets/jacket_014.png",
+    level: [9, 12, 18, 25, 29],
+    notes: [260, 489, 733, 976, 1206],
+  },
+  {
+    musicID: 15,
+    artistID: 4,
+    musicName: "PaⅢ.SENSATION",
+    jacketUrl: "/assets/jacket_015.png",
+    level: [8, 12, 17, 24, 29],
+    notes: [323, 394, 758, 1032, 1244],
+  },
+  {
+    musicID: 16,
+    artistID: 4,
+    musicName: "夜に駆ける",
+    jacketUrl: "/assets/jacket_016.png",
+    level: [6, 11, 18, 25, 29],
+    notes: [182, 357, 594, 932, 1140],
+  },
+  {
+    musicID: 17,
+    artistID: 4,
+    musicName: "Ready Steady",
+    jacketUrl: "/assets/jacket_017.png",
+    level: [5, 10, 16, 25, 29],
+    notes: [96, 238, 403, 564, 667],
+  },
+  {
+    musicID: 18,
+    artistID: 4,
+    musicName: "シネマ",
+    jacketUrl: "/assets/jacket_018.png",
+    level: [6, 12, 18, 24, 29],
+    notes: [105, 254, 465, 616, 800],
+  },
+  {
+    musicID: 19,
+    artistID: 4,
+    musicName: "Flyer!",
+    jacketUrl: "/assets/jacket_019.png",
+    level: [9, 12, 18, 24, 29],
+    notes: [157, 279, 404, 659, 1083],
+  },
+  {
+    musicID: 20,
+    artistID: 4,
+    musicName: "月光",
+    jacketUrl: "/assets/jacket_020.png",
+    level: [5, 12, 18, 25, 29],
+    notes: [261, 408, 598, 861, 805],
+  },
+  {
+    musicID: 21,
+    artistID: 4,
+    musicName: "劣等上等",
+    jacketUrl: "/assets/jacket_021.png",
+    level: [7, 12, 18, 25, 30],
+    notes: [148, 371, 520, 825, 952],
+  },
+  {
+    musicID: 22,
+    artistID: 4,
+    musicName: "夜咄ディセイブ",
+    jacketUrl: "/assets/jacket_022.png",
+    level: [8, 14, 19, 26, 30],
+    notes: [184, 424, 851, 1165, 1311],
+  },
+  {
+    musicID: 23,
+    artistID: 4,
+    musicName: "悪魔の踊り方",
+    jacketUrl: "/assets/jacket_023.png",
+    level: [5, 12, 19, 26, 30],
+    notes: [147, 290, 588, 792, 938],
+  },
+  {
+    musicID: 25,
+    artistID: 4,
+    musicName: "RAD DOGS",
+    jacketUrl: "/assets/jacket_025.png",
+    level: [6, 12, 18, 26, 30],
+    notes: [204, 418, 743, 1067, 1197],
+  },
+  {
+    musicID: 26,
+    artistID: 4,
+    musicName: "オルダーエゴ",
+    jacketUrl: "/assets/jacket_026.png",
+    level: [8, 12, 17, 26, 31],
+    notes: [301, 545, 787, 1175, 1557],
+  },
+  {
+    musicID: 27,
+    artistID: 4,
+    musicName: "ドクター＝ファンクビート",
+    jacketUrl: "/assets/jacket_027.png",
+    level: [8, 13, 19, 27, 32],
+    notes: [160, 436, 677, 984, 1198],
+  },
+  {
+    musicID: 28,
+    artistID: 4,
+    musicName: "チルドレンレコード",
+    jacketUrl: "/assets/jacket_028.png",
+    level: [9, 14, 19, 27, 32],
+    notes: [250, 564, 823, 1234, 1502],
+  },
+]
