1e3e59a4afe2c30ad687009bf4cb2da2ba85b558
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-10-01 21:23:11 +0900

    [mod]@tailwind/forms support

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,9 +1,10 @@
 import type { UserConfig } from "@unocss/core";
 import presetUno from "@unocss/preset-uno";
+import { presetForms } from "@unocss/preset-forms";
 
 // @ref https://github.com/unocss/unocss#configurations
 export default <UserConfig> {
-  presets: [presetUno()],
+  presets: [presetUno(), presetForms()],
   theme:{
     breakpoints: {
       lg: '1080px',
