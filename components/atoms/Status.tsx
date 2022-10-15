deb6a545cdc0c850b81c75115fe9958f26d1fa14
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-10-16 01:03:48 +0900

    [mod]update status color

--- components/atoms/Status.tsx
+++ components/atoms/Status.tsx
@@ -1,28 +1,23 @@
 import React from "react";
-import { ClearStatus, clearStatus } from "~/types/index.ts";
+import { STATUS, StatusValues } from "~/types/index.ts";
 
 export const Status = React.memo((props: Props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 20 20"
-  >
-    <polygon
-      points="0,10 10,0 20,10 10,20"
-      className={["stroke-3 stroke-slate-900", fillSet[props.status]].join(" ")}
-    />
-  </svg>
+    width="17"
+    height="17"
+    className={`rounded border-2 border-black ${fillSet[props.status]} rotate-45`}
+  />
 ));
 
 type Props = {
   class?: string;
-  status: ClearStatus;
+  status: StatusValues;
 };
 
 const fillSet = {
-  [clearStatus.NOPLAY]: "fill-slate-900",
-  [clearStatus.CLEAR]: "fill-yellow-300",
-  [clearStatus.FULL_COMBO]: "fill-fuchsia-300",
-  [clearStatus.ALL_PERFECT]: "fill-blue-400",
-};
+  [STATUS.NOPLAY]: "bg-slate-600",
+  [STATUS.CLEAR]: "bg-gradient-to-br from-yellow-300 to-yellow-500",
+  [STATUS.FULL_COMBO]: "bg-gradient-to-br from-fuchsia-300 to-fuchsia-400",
+  [STATUS.ALL_PERFECT]: "bg-gradient-to-br from-fuchsia-400 to-cyan-300",
+}
