7bd623444a5466a89e6220a4b66912f22d3a533d
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-10-17 01:11:17 +0900

    [mod]fix layout
    responsive design support

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -6,21 +6,21 @@ import { ThemeConsumer } from "~/hooks/useTheme.tsx";
 export const Footer = React.memo(() => (
   <ThemeConsumer>
     {({ darkMode }) => (
-      <div className="text-sm flex justify-between items-end">
-        <div className="flex">
+      <div className="text-sm md:flex grid justify-between items-end">
+        <div className="md:flex grid gap-y-2">
           <p>Copyright © 2022 ebiy0rom0. All Rights Reserved.</p>
           <Link
             to="/policy"
-            className={`ml-4 pl-4 border-l-2 ${
+            className={`md:ml-4 md:pl-4 md:border-l-2 ${
               darkMode ? "border-slate-200/40" : "border-slate-400/30"
             }`}
           >
             Privacy Policy
           </Link>
         </div>
-        <a href="https://github.com/ebiy0rom0/sekareco" className="hover:opacity-70 flex items-end">
+        <a href="https://github.com/ebiy0rom0/sekareco" className="hover:opacity-70 flex items-end mt-6 md:mt-0">
           <Icon icon={ICON_GITHUB} title="github" />
-          <p className="ml-2 hidden md:block">check to code →</p>
+          <p className="ml-2">check to code →</p>
         </a>
       </div>
     )}
