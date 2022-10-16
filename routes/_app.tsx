8a3e7324b02ae5eb496a83e4163889abc59a3774
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-10-16 11:22:30 +0900

    [mod]refactor
    reduced suspense area

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react"
+import { Suspense, useRef } from "react";
 import { useRouter } from "aleph/react";
 import { Head } from "aleph/react";
 import { Header } from "~/components/organisms/Header.tsx";
@@ -17,34 +17,33 @@ const MyApp: React.FC<Props> = ({ children }) => {
   // }
 
   return (
-    <React.Suspense fallback={<div className={`grid w-full h-full place-content-center`}>Loading...</div>}>
-      <ThemeProvider>
-        <ThemeConsumer>
-          {({ darkMode }) => (
-            <>
-              <Head>
-                <title>プロセカの記録帳</title>
-              </Head>
-              <div
-                className={`${
-                  darkMode ? "bg-slate-800 text-slate-400" : "bg-slate-100 text-slate-800"
-                }`}
-              >
-                {router.url.pathname !== "/"
-                  ? (
-                    <>
-                      <div
-                        className={`sticky top-0 z-20 w-full flex-none border-b ${
-                          darkMode ? "border-slate-200/40" : "border-slate-400/30"
-                        } backdrop-blur-md`}
-                      >
-                        <div className="max-w-[110rem] px-10 py-2 mx-auto">
-                          <Header />
-                        </div>
+    <ThemeProvider>
+      <ThemeConsumer>
+        {({ darkMode }) => (
+          <>
+            <Head>
+              <title>プロセカの記録帳</title>
+            </Head>
+            <div
+              className={`${
+                darkMode ? "bg-slate-800 text-slate-400" : "bg-slate-100 text-slate-800"
+              }`}
+            >
+              {router.url.pathname !== "/"
+                ? (
+                  <>
+                    <div
+                      className={`sticky top-0 z-20 w-full flex-none border-b ${
+                        darkMode ? "border-slate-200/40" : "border-slate-400/30"
+                      } backdrop-blur-md`}
+                    >
+                      <div className="max-w-[110rem] px-10 py-2 mx-auto">
+                        <Header />
                       </div>
-                      <div className="max-w-[100rem] mx-auto py-8">
-                        <div
-                          className={`
+                    </div>
+                    <div className="max-w-[100rem] mx-auto py-8">
+                      <div
+                        className={`
                           fixed
                           box-border
                           right-[max(0px,calc(50%-50rem))]
@@ -56,53 +55,72 @@ const MyApp: React.FC<Props> = ({ children }) => {
                           2xl:block
                           border-l ${darkMode ? "border-slate-200/40" : "border-slate-400/30"}
                         `}
-                        >
-                          <Navigation />
-                        </div>
+                      >
+                        <Navigation />
+                      </div>
+                      <Suspense
+                        fallback={
+                          <div className={`grid w-full h-full place-content-center`}>
+                            Loading...
+                          </div>
+                        }
+                      >
                         <div className="2xl:mr-[15rem] px-2 lg:px-15 2xl:px-8 items-center">
                           <div className="mx-5">
                             {children}
                           </div>
                           <div
                             className={`
-                            border-t ${darkMode ? "border-slate-200/40" : "border-slate-400/30"}
-                            mt-10 pt-15 pb-7
-                          `}
+                              border-t ${darkMode ? "border-slate-200/40" : "border-slate-400/30"}
+                              mt-10 pt-15 pb-7 mx-30
+                            `}
                           >
                             <Footer />
                           </div>
                         </div>
+                      </Suspense>
+                    </div>
+                  </>
+                )
+                : (
+                  <div className="relative mx-auto w-full h-[100vh] items-center overflow-hidden">
+                    <div
+                      className={`relative flex flex-col w-1/2 items-center h-full z-10 ${
+                        darkMode ? "bg-slate-800" : "bg-slate-100"
+                      }`}
+                    >
+                      <div className="flex flex-col justify-end min-h-[77vh] animated animated-fade-in z-100">
+                        {children}
                       </div>
-                    </>
-                  )
-                  : (
-                    <div className="relative mx-auto w-full h-[100vh] items-center overflow-hidden">
-                      <div className={`relative flex flex-col w-2/5 bg-slate-100 items-center z-10 h-full`}>
-                        <div className="flex flex-col justify-end min-h-[77vh] animated animated-fade-in">
-                          {children}
-                        </div>
-                        <div
-                          className={`
+                      <div
+                        className={`
                           mt-20 pt-15 pb-10
                           w-min-full
                         `}
-                        >
-                          <Footer />
-                        </div>
-                        <svg className="absolute inset-y-0 -right-0 hidden h-full w-60 fill-slate-100 translate-x-1/2 transform lg:block" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-                          <polygon points="50,0 100,0 50,100 0,100"></polygon>
-                        </svg>
+                      >
+                        <Footer />
                       </div>
-                      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-2/3 bg-slate-300">
-                      </div>
+                      <svg
+                        className={`absolute inset-y-0 -right-0 hidden h-full w-60 translate-x-1/2 transform lg:block ${
+                          darkMode ? "fill-slate-800" : "fill-slate-100"
+                        }`}
+                        fill="currentColor"
+                        viewBox="0 0 100 100"
+                        preserveAspectRatio="none"
+                        aria-hidden="true"
+                      >
+                        <polygon points="50,0 100,0 50,100 0,100"></polygon>
+                      </svg>
                     </div>
-                  )}
-              </div>
-            </>
-          )}
-        </ThemeConsumer>
-      </ThemeProvider>
-    </React.Suspense>
+                    <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-2/3 bg-slate-500 animated animated-fade-in-up animated-delay-1s">
+                    </div>
+                  </div>
+                )}
+            </div>
+          </>
+        )}
+      </ThemeConsumer>
+    </ThemeProvider>
   );
 };
 
