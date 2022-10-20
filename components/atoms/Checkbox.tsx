ec8e5f090dcd8c8548a8f2a99ab278f5de59e02c
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-10-21 01:04:06 +0900

    [mod]💄change design
    back ground color setting in dark mode

--- components/atoms/Checkbox.tsx
+++ components/atoms/Checkbox.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import { ThemeConsumer } from "~/hooks/useTheme.tsx";
 
 export const Checkbox = <T extends string | number>(props: Props<T>) => (
-  <div className="flex items-center">
-    <input
-      id={props.id}
-      type="checkbox"
-      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-red-300"
-      value={props.value}
-      checked={props.checked}
-      onChange={(e) => props.handler(e.target.value)}
-    />
-    <label htmlFor={props.id} className="ml-3 text-sm">{props.children}</label>
-  </div>
-);
+  <ThemeConsumer>
+    {({darkMode}) => (
+      <div className="flex items-center">
+        <input
+          id={props.id}
+          type="checkbox"
+          className={`h-4 w-4 rounded ${darkMode ? "bg-slate-700 border-gray-500" : "border-gray-300"} text-indigo-600 focus:outline-none focus:ring-2 focus:border-indigo-400`}
+          value={props.value}
+          checked={props.checked}
+          onChange={(e) => props.handler(e.target.value)}
+        />
+        <label htmlFor={props.id} className="ml-3 text-sm">{props.children}</label>
+      </div>
+    )}
+  </ThemeConsumer>
+  );
 
 type Props<T> = {
   id: string;
