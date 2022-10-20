d4840c501e7ef0ea5bfa68ea75f1e55cd7bbeb37
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2022-10-21 02:57:07 +0900

    [mod]refactor
    delete unnecessary block

--- routes/index.tsx
+++ routes/index.tsx
@@ -12,65 +12,61 @@ const Index: React.FC = () => {
   return (
     <ThemeConsumer>
       {({ darkMode, switchMode }) => (
-        <>
-          <div className="grid gap-y-5 mx-auto">
-            <div className="flex gap-x-5">
-              {"プロセカの".split("").map((s, i) => (
-                <span
-                  key={i.toString()}
-                  className={`text-5xl font-bold
-                    ${
-                    i == 0 ? " text-cyan-400" : darkMode ? " text-slate-300" : " text-slate-600"
-                  }`}
-                >
-                  {s}
-                </span>
-              ))}
-            </div>
-            <div className="flex gap-x-5 flex-row-reverse mt-5 -mr-5">
-              {"記録帳".split("").reverse().map((s, i) => (
-                <span
-                  key={i.toString()}
-                  className={`text-5xl font-bold
-                    ${
-                    i == 2 ? " text-pink-500/90" : darkMode ? " text-slate-300" : " text-slate-600"
-                  }`}
-                >
-                  {s}
-                </span>
-              ))}
-            </div>
-            <div className="min-w-[25.0rem]">
-              <SignInForm />
-              <button
-                type="button"
-                className="
-                  mt-7
-                  border-none
-                  bg-transparent
-                  text-lg text-slate-400
-                  justify-self-end
-                "
-                onClick={open}
+        <div className="grid gap-y-5 mx-atuo">
+          <div className="flex gap-x-5">
+            {"プロセカの".split("").map((s, i) => (
+              <span
+                key={i.toString()}
+                className={`text-5xl font-bold
+                  ${i == 0 ? " text-cyan-400" : darkMode ? " text-slate-300" : " text-slate-600"}`}
               >
-                click here to sign up.
-              </button>
-              <div className="flex flex-row-reverse mt-5 gap-x-9">
-                <Icon icon={ICON_GITHUB} />
-                <Toggle
-                  mode={!darkMode}
-                  style={ToggleStyle.THEME}
-                  role={switchMode}
-                />
-              </div>
-            </div>
-            {render(
-              <div className="min-w-[23.0rem]">
-                <SignUpForm />
-              </div>,
-            )}
+                {s}
+              </span>
+            ))}
           </div>
-        </>
+          <div className="flex gap-x-5 flex-row-reverse mt-5">
+            {"記録帳".split("").reverse().map((s, i) => (
+              <span
+                key={i.toString()}
+                className={`text-5xl font-bold
+                  ${
+                  i == 2 ? " text-pink-500/90" : darkMode ? " text-slate-300" : " text-slate-600"
+                }`}
+              >
+                {s}
+              </span>
+            ))}
+          </div>
+          <div className="min-w-[25.0rem]">
+            <SignInForm />
+            <button
+              type="button"
+              className="
+                mt-7
+                border-none
+                bg-transparent
+                text-lg text-slate-400
+                justify-self-end
+              "
+              onClick={open}
+            >
+              click here to sign up.
+            </button>
+            <div className="flex flex-row-reverse mt-5 gap-x-9">
+              <Icon icon={ICON_GITHUB} />
+              <Toggle
+                mode={!darkMode}
+                style={ToggleStyle.THEME}
+                role={switchMode}
+              />
+            </div>
+          </div>
+          {render(
+            <div className="min-w-[23.0rem]">
+              <SignUpForm />
+            </div>,
+          )}
+        </div>
       )}
     </ThemeConsumer>
   );
