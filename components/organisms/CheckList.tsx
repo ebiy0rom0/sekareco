e3d282552da0052863a55e78a427d2422e37d3d3
Kenya Kojima <kk.n0h2a2l3.end@gmail.com>
2023-02-02 22:23:43 +0900

    [add]generalization filter

---
+++ components/organisms/CheckList.tsx
@@ -0,0 +1,49 @@
+import { Disclosure } from "@headlessui/react";
+import { Checkbox } from "~/components/atoms/Checkbox.tsx";
+import { Icon, ICON_MINUS, ICON_PLUS } from "~/components/atoms/Icon.tsx";
+import { ThemeConsumer } from "~/hooks/useTheme.tsx";
+
+export const CheckList = (props: Props) => {
+  return (
+    <ThemeConsumer>
+      {({ darkMode }) => (
+        <>
+          <div className={`py-6 border-b ${darkMode ? "border-gray-700" : "border-gray-200"}`}>
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button className="w-full pr-3">
+                    <div className="flex justify-between items-center">
+                      <span className="font-semibold tracking-widest">{ props.title }</span>
+                      <Icon icon={open ? ICON_MINUS : ICON_PLUS} />
+                    </div>
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="space-y-4 pt-6">
+                    {Object.entries(props.items).map(([k, v]) => (
+                      <Checkbox
+                        key={k.toString()}
+                        id={`checklist-${k}`}
+                        handler={props.handler}
+                        value={v}
+                        checked={props.isChecked(v)}
+                      >
+                        { k }
+                      </Checkbox>
+                    ))}
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          </div>
+        </>
+      )}
+    </ThemeConsumer>
+  );
+};
+
+export type Props = {
+  title: string;
+  items: {string: number};
+  handler: (n: string) => void;
+  isChecked: (n: number) => boolean;
+};
