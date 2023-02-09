cb333dcc999e3dc0ddebb55234d82a783aea91f9
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-02-09 19:19:14 +0900

    [mod]scale fit support

--- components/atoms/Image.tsx
+++ components/atoms/Image.tsx
@@ -8,16 +8,16 @@ export const Image: FC<Props> = ({ url, alt }) => {
     <>
       <img
         src={`C:/Users/user/Documents/wk/sekareco/sekareco_assets${url}`}
-        className={`rounded lg:rounded-l w-[150px] lg:w-[70px] ${preload ? "" : "hidden"}`}
-        onError={(e) => (e.target as HTMLImageElement).src = "assets/logo.svg"}
+        className={`rounded lg:rounded-l object-fill h-full w-full ${preload ? "" : "hidden"}`}
+        onError={(e) => (e.target as HTMLImageElement).src = "assets/goooooooooooooooo.png"}
         onLoad={() => setPreload(true)}
         alt={alt}
       />
-        {!preload && (
-          <div className="rounded h-[70px] w-[70px] flex items-center justify-center bg-slate-500 pb-4">
-            <Spinner />
-          </div>
-        )}
+      {!preload && (
+        <div className="rounded-l object-fill w-full h-full flex items-center justify-center bg-slate-500">
+          <Spinner />
+        </div>
+      )}
     </>
   )
 }
