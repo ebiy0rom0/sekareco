9f839f3f478dcc006feac6c254e79e6bdd0eb6e2
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-02-10 23:04:19 +0900

    [mod]fix props

--- components/organisms/ScoreCard.tsx
+++ components/organisms/ScoreCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { memo, useEffect, useRef, useState } from "react";
 import { ThemeConsumer } from "~/hooks/useTheme.tsx";
 import { DifficultyValues, STATUS, StatusValues } from "~/types/index.ts";
 import { Status } from "~/components/atoms/Status.tsx";
@@ -6,7 +6,7 @@ import { Score } from "~/components/atoms/Score.tsx";
 import { Difficulty as DiffComponent } from "~/components/atoms/Difficulty.tsx";
 import { Image } from "~/components/atoms/Image.tsx";
 
-export const ScoreCard = React.memo((props: Props) => {
+export const ScoreCard = memo<Props>(({ music, diff, status, score, filter }) => {
   const [view, setView] = useState(true);
   const recordRef = useRef<HTMLDivElement>(null);
 
@@ -46,33 +46,33 @@ export const ScoreCard = React.memo((props: Props) => {
               divide-zinc-300/60"
           }`}
         >
-          <div className="music__master w-70 lg:w-25">
-            <Image url={props.jacketUrl} alt={props.title} />
+          <div className="music__master w-70 xl:w-20">
+            <Image url={music.jacketUrl} alt={music.title} />
           </div>
           <div className="music__record relative w-full h-full content-end grid grid-cols-2 lg:grid-cols-1 px-2 py-3 gap-y-1">
             <label className="absolute break-all inset-x-0 top-0 rounded-tr bg-slate-500/55 font-bold text-sm text-slate-300 indent-1.5 px-1">
-              {props.title}
+              {music.title}
             </label>
             <div className="difficulty grid grid-cols-none lg:grid-cols-5 auto-cols-fr gap-y-2 mt-5">
-              {Object.values(props.filter).map((v) => (
+              {Object.values(filter).map((v) => (
                 <div key={v.toString()}>
                   <DiffComponent
                     difficulty={v}
-                    level={props.level[v]}
+                    level={music.level[v]}
                   />
                 </div>
               ))}
             </div>
-            <div className="record grid grid-cols-none lg:grid-cols-5 auto-cols-fr gap-y-2 mt-4">
-              {Object.values(props.filter).map((v) => (
+            <div className="record grid grid-cols-none lg:grid-cols-5 auto-cols-fr gap-y-2">
+              {Object.values(filter).map((v) => (
                 <div
                   key={v.toString()}
                   className="h-[20px] min-w-fit grid grid-flow-col -ml-4 justify-center content-bottom gap-1"
                 >
                   <div className="w-28 mt-1 mr-1">
-                    <Score score={props.score[v]} notes={props.notes[v]} diff={props.diff} />
+                    <Score score={score[v]} notes={music.notes[v]} diff={diff} />
                   </div>
-                  <Status status={props.status[v] ?? STATUS.NOPLAY} />
+                  <Status status={status[v] ?? STATUS.NOPLAY} />
                 </div>
               ))}
             </div>
@@ -84,12 +84,9 @@ export const ScoreCard = React.memo((props: Props) => {
 });
 
 type Props = {
-  title: string;
-  jacketUrl: string;
+  music: Music;
   diff: boolean;
   status: StatusValues[];
   score: number[];
   filter: DifficultyValues[];
-  level: number[];
-  notes: number[];
 };
