51b186a1fde52174afb19c613b2d112147702047
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-02-10 23:47:18 +0900

    [mod]fix types

--- hooks/useCheckGroup.tsx
+++ hooks/useCheckGroup.tsx
@@ -1,12 +1,12 @@
 import { useCallback, useState } from "react";
 import { Checkbox } from "~/components/atoms/Checkbox.tsx";
 
-export const useCheckGroup = (lists: {[label: string]: string | number}) => {
+export const useCheckGroup = <T extends string | number>(lists: {[label: string]: T}): [T[], () => JSX.Element] => {
   const [checkList, setCheckList] = useState(() => Object.keys(lists).map(() => true))
 
   const handler = (index: number) => setCheckList((prev) => prev.map((c, i) => (i === index ? !c : c)))
 
-  const checkedValues = useCallback((): Values<typeof lists>[] => Object.values(lists).filter((_, i) => checkList[i]), [checkList])
+  const checkedValues = Object.values(lists).filter((_, i) => checkList[i])
 
   const render = (): JSX.Element => (
     <>
