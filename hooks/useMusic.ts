a473d504cd9b018d5376e1359d9f4a529d0a5af3
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-02-12 08:09:07 +0900

    [mod]change return type
    level returns tuple array
    fix to level upper and lower calculation

--- hooks/useMusic.ts
+++ hooks/useMusic.ts
@@ -1,14 +1,11 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { apiFactory } from "~/api/apiFactory.ts";
 import { DifficultyValues } from "~/types/index.ts";
 
 // custom hook
 export const useMusic = (): [
   Music[],
-  (d: DifficultyValues) => {
-    lower: number;
-    upper: number;
-  },
+  (d: DifficultyValues) => [number, number],
 ] => {
   const [music, setMusic] = useState<Music[]>([]);
 
@@ -20,21 +17,16 @@ export const useMusic = (): [
   }, []);
 
   // all music level array
-  const getLevelListByDifficulty = useCallback(
-    (difficulty: number) => [...music.map((m) => m.level[difficulty])],
-    [music],
-  );
+  const levelRange = (difficulty: number) => music.length === 0 ? [0] : [...new Set(music.map((m) => m.level[difficulty]))]
 
   // exist level min & max
-  const getLevelUpper = (difficulty: number) =>
-    Math.max(0, ...getLevelListByDifficulty(difficulty));
-  const getLevelLower = (difficulty: number) =>
-    Math.min(100, ...getLevelListByDifficulty(difficulty));
+  const levelLower = (difficulty: number) => Math.min(...levelRange(difficulty));
+  const levelUpper = (difficulty: number) => Math.max(...levelRange(difficulty));
 
   return [music, (d: DifficultyValues) => {
-    return {
-      lower: getLevelLower(d),
-      upper: getLevelUpper(d),
-    };
+    return [
+      levelLower(d),
+      levelUpper(d),
+    ];
   }];
 };
