67e6e24063d5f36e58fe3780475f9da2cb8a4ff1
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-02-12 08:09:27 +0900

    [add]group manged hook

---
+++ hooks/useGroup.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect, useMemo } from "react";
+import { apiFactory } from "~/api/apiFactory.ts";
+
+export const useGroup = (): [Group[], {[s: string]: number}] => {
+  const [ group, setGroup ] = useState<Group[]>([])
+
+  useEffect(() => {
+    (async () => {
+      const list = await apiFactory.get("music").getGroupList();
+      setGroup(list);
+    })();
+  }, []);
+
+  const groupkv = useMemo(() => group.reduce((prev, g) => {
+    return { ...prev, [g.groupName]: g.groupID }
+  }, {}), [group])
+
+  return [group, groupkv];
+}
