c590129e4c4ae7ca1294e3cc456e7c19f4eab239
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-02-12 16:07:51 +0900

    [mod]change name

--- components/organisms/MusicCard.tsx
+++ components/organisms/MusicCard.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { Image } from "~/components/atoms/Image.tsx";
 
-export const Music = React.memo((props: Props) => {
-
+export const MusicCard = React.memo((props: Props) => {
   return (
     <div className="relative items-center h-full">
-      <label className="absolute break-all inset-x-0 bottom-0 rounded-bl bg-white/65 font-bold text-sm text-slate-800 indent-1.5">
+      {
+        /* <label className="absolute break-all inset-x-0 bottom-0 rounded-bl bg-white/65 font-bold text-sm text-slate-800 indent-1.5">
         {props.title}
-      </label>
+      </label> */
+      }
       <Image url={props.url} alt={props.title} />
     </div>
   );
