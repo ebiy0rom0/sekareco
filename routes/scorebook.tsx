1554104b15cfbc118fac207122b70b4db473249c
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-02-12 15:58:24 +0900

    [add]level filter support

--- routes/scorebook.tsx
+++ routes/scorebook.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import { DIFFICULTY, StatusValues } from "~/types/index.ts";
+import { FC, useState } from "react";
+import { DIFFICULTY, DifficultyValues, StatusValues } from "~/types/index.ts";
 import { apiFactory } from "~/api/apiFactory.ts";
 import { ICON_FILTER, ICON_SORT } from "~/components/atoms/Icon.tsx";
 import { Checkbox } from "~/components/atoms/Checkbox.tsx";
+import { Selectbox } from "~/components/atoms/Selectbox.tsx";
 import { ScoreCard } from "~/components/organisms/ScoreCard.tsx";
 import { RecordEditor } from "~/components/organisms/RecordEditor.tsx";
 import { Popover } from "~/components/organisms/Popover.tsx";
@@ -15,30 +16,40 @@ import { useRecordEditor } from "~/hooks/useRecordEditor.ts";
 import { useMultiRange } from "~/hooks/useMultiRange.tsx";
 import { useModal } from "~/hooks/useModal.tsx";
 import { ThemeConsumer } from "~/hooks/useTheme.tsx";
-import { useI18n } from "../hooks/useI18n.ts";
+import { useI18n } from "~/hooks/useI18n.ts";
 import { useFilter } from "~/hooks/useFilter.ts";
 import { useSort } from "~/hooks/useSort.ts";
-import { useObjectCompare } from "~/utils/useObjectCompare.ts";
 import { useCheckGroup } from "~/hooks/useCheckGroup.tsx";
+import { useObjectCompare } from "~/utils/useObjectCompare.ts";
 
-const ScoreBook: React.FC = () => {
+const ScoreBook: FC = () => {
   const { t } = useI18n();
   const [music, levelRange] = useMusic();
   const [group, groupkv] = useGroup();
   const { getRecord, setRecord } = useRecord(1);
 
-  const sortKeys: {[target: string]: Keys<Music>} = {[t.TARGET_DEFAULT]: "musicID", [t.TARGET_LEVEL]: "level", [t.TARGET_UNIT]: "groupID"}
-  const sortOrders: {[target: string]: boolean} = {[t.ORDER_ASC]: true, [t.ORDER_DESC]: false}
-  const [sortKey, setSortKey] = useState(Object.keys(sortKeys)[0])
-  const [sortOrder, setSortOrder] = useState(Object.keys(sortOrders)[0])
+  const [difficulty, setDifficulty] = useState<DifficultyValues>(DIFFICULTY.MASTER);
+  const sortKeys: { [target: string]: Keys<Music> } = {
+    [t.TARGET_DEFAULT]: "musicID",
+    [t.TARGET_LEVEL]: "level",
+    [t.TARGET_UNIT]: "groupID",
+  };
+  const sortOrders: { [target: string]: boolean } = { [t.ORDER_ASC]: true, [t.ORDER_DESC]: false };
+  const [sortKey, setSortKey] = useState(Object.keys(sortKeys)[0]);
+  const [sortOrder, setSortOrder] = useState(Object.keys(sortOrders)[0]);
 
-  const [low, high, renderLevelMultiRange] = useMultiRange(...levelRange(4))
-  const [showGroupIDs, renderGroupIDCheckGroup] = useCheckGroup(groupkv)
-  const [showDifficulty, renderDifficultyCheckGroup] = useCheckGroup(DIFFICULTY)
+  const [low, high, renderLevelMultiRange] = useMultiRange(...levelRange(difficulty));
+  const [showGroupIDs, renderGroupIDCheckGroup] = useCheckGroup(groupkv);
+  const [showDifficulty, renderDifficultyCheckGroup] = useCheckGroup(DIFFICULTY);
 
-  const levelMap = [...new Set([...Array(Math.abs(high - low + 1))].map((_,i) => low + i))]
-  const [ filtered ] = useFilter(music, [["groupID", showGroupIDs], ["level", {4: levelMap}]])
-  const [ sorted ] = useSort(filtered, [[sortKeys[sortKey], sortOrders[sortOrder]]])
+  const [filtered] = useFilter(music, [
+    ["groupID", showGroupIDs],
+    ["level", { [difficulty]: [...new Set([...Array(high - low + 1)].map((_, i) => low + i))] }],
+  ]);
+  const [sorted] = useSort(filtered, [[
+    sortKeys[sortKey],
+    sortKeys[sortKey] === "level" ? { [difficulty]: sortOrders[sortOrder] } : sortOrders[sortOrder],
+  ]]);
 
   // score display mode[raw data or max minus]
   const [maxMinus, setMaxMinus] = useState(false);
@@ -91,7 +102,7 @@ const ScoreBook: React.FC = () => {
                 darkMode ? "first-letter:text-pink-500/90" : "first-letter:text-cyan-400"
               }`}
             >
-              { t.SCORE_BOOK }
+              {t.SCORE_BOOK}
             </h2>
             <div className="flex items-end gap-x-8 mr-4 z-10">
               <div className="font-semibold">
@@ -101,41 +112,71 @@ const ScoreBook: React.FC = () => {
                   handler={(_: string) => setMaxMinus(!maxMinus)}
                   value={""}
                 >
-                  MAX -
+                  MAX－
                 </Checkbox>
               </div>
-                <Popover icon={ICON_SORT}>
-                  <div className="w-30">
-                    <RadioGroup label={t.SORT_TARGET} items={Object.keys(sortKeys)} value={sortKey} onChange={setSortKey} />
-                    <RadioGroup label={t.SORT_ORDER} items={Object.keys(sortOrders)} value={sortOrder} onChange={setSortOrder} />
-                  </div>
-                </Popover>
-              <div className="flex sm:hidden">
+              <Popover icon={ICON_SORT}>
+                <div className="w-30">
+                  <RadioGroup
+                    label={t.SORT_TARGET}
+                    items={Object.keys(sortKeys)}
+                    value={sortKey}
+                    onChange={setSortKey}
+                  />
+                  <RadioGroup
+                    label={t.SORT_ORDER}
+                    items={Object.keys(sortOrders)}
+                    value={sortOrder}
+                    onChange={setSortOrder}
+                  />
+                </div>
+              </Popover>
+              <div className="flex lg:hidden">
                 <Popover icon={ICON_FILTER}>
-                  <div className="w-42">
-                    <div className="grid">
-                      { renderLevelMultiRange() }
+                  <div className="w-56">
+                    <div
+                      className={`grid gap-y-5 px-2 pt-6 lg:pt-4 pb-15 ${
+                        darkMode ? "border-gray-700" : "border-gray-200"
+                      }`}
+                    >
+                      <Selectbox
+                        options={DIFFICULTY}
+                        selected={difficulty}
+                        onChange={(select: string) => setDifficulty(+select as DifficultyValues)}
+                      />
+                      {renderLevelMultiRange()}
                     </div>
                     <Disclosure title={t.GROUP}>
-                      { renderGroupIDCheckGroup() }
+                      {renderGroupIDCheckGroup()}
                     </Disclosure>
-                      <Disclosure title={t.DIFFICULTY}>
-                        { renderDifficultyCheckGroup() }
-                      </Disclosure>
-                    </div>
-                  </Popover>
-                </div>
+                    <Disclosure title={t.DIFFICULTY}>
+                      {renderDifficultyCheckGroup()}
+                    </Disclosure>
+                  </div>
+                </Popover>
+              </div>
             </div>
           </div>
           <div className="flex flex-col">
-            <div className="grid grid-cols-1 xl:grid-cols-5 gap-x-15 py-5">
+            <div className="relative grid grid-cols-1 xl:grid-cols-5 gap-x-15 py-5">
               <form className="hidden lg:block divide-y">
-                { renderLevelMultiRange() }
+                <div
+                  className={`grid gap-y-5 px-2 pt-6 lg:pt-4 pb-15 ${
+                    darkMode ? "border-gray-700" : "border-gray-200"
+                  }`}
+                >
+                  <Selectbox
+                    options={DIFFICULTY}
+                    selected={difficulty}
+                    onChange={(select: string) => setDifficulty(+select as DifficultyValues)}
+                  />
+                  {renderLevelMultiRange()}
+                </div>
                 <Disclosure title={t.GROUP}>
-                  { renderGroupIDCheckGroup() }
+                  {renderGroupIDCheckGroup()}
                 </Disclosure>
                 <Disclosure title={t.DIFFICULTY}>
-                  { renderDifficultyCheckGroup() }
+                  {renderDifficultyCheckGroup()}
                 </Disclosure>
               </form>
               <div className="w-full xl:col-span-4 place-self-start grid grid-cols-1 md:grid-cols-2 lg:grid-cols-none gap-y-3 gap-x-5">
